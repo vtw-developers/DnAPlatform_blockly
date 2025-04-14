de4c00fb37ce39a1579528c74b36766087ba0e7f
jinhee-han <jinhjee.han@vtw.co.kr>
2025-04-14 00:12:49 -0400

    파일 정리리

--- src/pages/auth/ProfilePage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UserProfile from '../../components/auth/UserProfile';
-
-const ProfilePage = () => {
-  return <UserProfile />;
-};
-
-export default ProfilePage; 
--- src/pages/auth/UserManagementPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UserManagement from '../../components/auth/UserManagement';
-
-const UserManagementPage = () => {
-  return <UserManagement />;
-};
-
-export default UserManagementPage; 
